a0e87b4f23a8ecdfbab699169d5c9329eb7a9529
naielv16 <109038805+naielv@users.noreply.github.com>
2024-07-07 19:08:29 +0200

    update

--- programs/filemanager/filemanager.js
+++ programs/filemanager/filemanager.js
@@ -4,8 +4,10 @@ Liscense: MIT
 Author: naielv
 Date: 2024-07-07
 */
+
+var UID = system.guid();
 new explorer.window()
-.resize(640, 480)
+.resize(350, 480)
 .center()
 .title('Archivos')
 .icon('programs/filemanager/logo.ico')
@@ -14,6 +16,6 @@ new explorer.window()
 	var bod = this.body;
 	bod
 	.css({'font-size':'10px'})
-	.html(`<div id='filebrowserd'></div>`);
-	bod.find('div#filebrowserd').html("hehe")
+	.html(`<table><thead><tr><th colspan="2">Nombre</th><th>Tipo</th></tr></thead><tbody border="1" id='`+UID+`'></tbody></table>`);
+	load_folder(UID, "")
 });
